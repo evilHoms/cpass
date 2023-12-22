ffa340a0a66a128f9e00a1fc22185cb33d3316ba
evilHoms <homspage@gmail.com>
2023-12-22 15:18:03 +0100

    Add install script

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+python3 -m pip install -r requirements.txt
+
+cat > cpass <<'EOF'
+#!/bin/bash
+$PWD/main.py $@
+EOF
+
+chmod +x $PWD/cpass
+
+echo "Dependencies installed"
+echo "cpass script generated, put it to your PATH dirrectory."
